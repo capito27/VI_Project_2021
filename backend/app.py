5556f5db3d89fc4002ba77d19e1310dfcd7d72ca
Guillaume Dévaud <gdvaud@gmail.com>
2020-11-30 01:24:08 +0100

    Adding view per week

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,13 @@
 from flask import Flask, jsonify
 import numpy as np
 
-from data import ratings
+from data import ratings, movies
 
 app = Flask(__name__)
 
 
-def test():
-    data = np.zeros(12)
-
-    months = ratings.timestamp // 2629743 % 12
-    for i in range(0, 12):
-        data[i] = len(months[months == i])
-
-    return data
-
-
-@app.route('/')
-def home():
+@app.route('/views/per_month')
+def views_per_month():
     values = {
         "labels": [
             "January",
@@ -33,8 +23,26 @@ def home():
             "November",
             "December",
         ],
-        "data": test().tolist()
+        "data": np.zeros(12).tolist()
     }
+
+    months = ratings.timestamp // 2629743 % 12
+    for i in range(0, 12):
+        values["data"][i] = len(months[months == i])
+
+    return jsonify(values)
+
+
+@app.route('/views/per_week')
+def views_per_week():
+    values = {
+        "labels": np.arange(1, 53).tolist(),
+        "data": np.zeros(52).tolist()
+    }
+
+    weeks = ratings.timestamp // 86400 // 7 % 52
+    for i in range(0, 52):
+        values["data"][i] = len(weeks[weeks == i])
 
     return jsonify(values)
 
